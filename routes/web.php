3de0118adda0e89d0a74b4cd585914e65383375f
HilmiIrfan <hilminaafiudin@gmail.com>
2024-02-23 10:18:15 +0700

    Pertemuan 2 (Routing)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,73 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Basic Routing*/
+Route::get('/hello', function(){
+    return 'Hello World';
+});
+
+Route::get('/world', function(){
+    return 'World';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return "Hilmi Irfan Naafi'udin / 2241720196";
+});
+
+/*Route Parameters*/
+Route::get('/user/{name}', function($name){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+/*Optional Parameter*/
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya '.$name;
+});
+
+/*Route Group
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+});
+Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Route Prefixes
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+Redirect Routes
+Route::redirect('/here', '/there');
+
+View Routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+*/
